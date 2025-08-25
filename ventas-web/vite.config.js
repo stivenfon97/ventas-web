4b7e009759da09a37f51dd791faf28b81b3a4528
stiven fonseca <stivenfon2u@gmail.com>
2025-08-25 09:13:03 -0500

    update

--- ventas-web/vite.config.js
+++ ventas-web/vite.config.js
@@ -10,4 +10,16 @@ export default defineConfig({
         }),
         react(),
     ],
+    server:{
+        host: true,
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true,
+        }
+    }
 });
+
+
+ 
